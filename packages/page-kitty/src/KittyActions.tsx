6940f8cea28ccf8a85a1f35ce0cebe3a7607de42
Bryan Chen <xlchen1291@gmail.com>
2020-05-31 22:05:53 +1200

    Able to breed kitty

--- packages/page-kitty/src/KittyActions.tsx
+++ packages/page-kitty/src/KittyActions.tsx
@@ -2,29 +2,55 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import React from 'react';
-import { Button, TxButton } from '@polkadot/react-components';
+import React, { useState } from 'react';
+import BN from 'bn.js';
+import { Button, TxButton, InputNumber } from '@polkadot/react-components';
 
- type Props = {
-   accountId: string | null
- };
+type Props = {
+  accountId: string | null
+};
 
-const KittyActions: React.FC<Props> = ({ accountId }: Props) => (
-  <section>
-    <h1>Kitty Actions</h1>
-    <div className='ui--row'>
-      <div className='large'>
-        <Button.Group>
-          <TxButton
-            accountId={accountId}
-            label='Create New Kitty'
-            params={[]}
-            tx='kitties.create'
-          />
-        </Button.Group>
+const KittyActions: React.FC<Props> = ({ accountId }: Props) => {
+  const [parent1, setParent1] = useState<BN | undefined>(undefined);
+  const [parent2, setParent2] = useState<BN | undefined>(undefined);
+
+  return (
+    <section>
+      <h1>Kitty Actions</h1>
+      <div className='ui--row'>
+        <div className='large'>
+          <Button.Group>
+            <TxButton
+              accountId={accountId}
+              label='Create New Kitty'
+              params={[]}
+              tx='kitties.create'
+            />
+          </Button.Group>
+        </div>
       </div>
-    </div>
-  </section>
-);
+      <div className='ui--row'>
+        <div className='large'>
+          <InputNumber
+            label='First Parent Kitty ID'
+            onChange={setParent1}
+          />
+          <InputNumber
+            label='Second Parent Kitty ID'
+            onChange={setParent2}
+          />
+          <Button.Group>
+            <TxButton
+              accountId={accountId}
+              label='Breed Kitty'
+              params={[parent1, parent2]}
+              tx='kitties.breed'
+            />
+          </Button.Group>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default KittyActions;
